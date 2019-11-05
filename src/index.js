05f28e19d4c009abda08b42673714f362b54431f
Winther Kristian <Kristian.Winther@norconsult.com>
2019-11-05 22:20:28 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from "react-router-dom";
+// import { AzureAD } from 'react-aad-msal';
+// import { authProvider } from './services/authProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  // <AzureAD provider={authProvider} forceLogin={true}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    // </AzureAD>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
